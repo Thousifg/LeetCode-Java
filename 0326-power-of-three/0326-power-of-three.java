a1ba668b53a7aedb4b3de8b167a39e2f868af673
Gangavaram Thousif <91539305+Thousifg@users.noreply.github.com>
2023-06-24 12:36:44 +0530

    Time: 15 ms (99.96%), Space: 43.2 MB (22.68%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n == 0){
-            return false;
+        while(n > 1){
+            if(n % 3 != 0){
+                return false;
+            }
+            n=n/3;
         }
-        if(n == 1){
-            return true;
-        }
-        return n%3 == 0 ? isPowerOfThree(n/3) : false;
+        return (n>0);
     }
 }
