b94cb7c6af68596416a04aadb39d0f9c19218cd9
Gangavaram Thousif <91539305+Thousifg@users.noreply.github.com>
2023-08-28 08:59:49 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (64.97%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -3,38 +3,44 @@ class MyStack {
     private Queue<Integer> primaryQueue;
     private Queue<Integer> secondaryQueue;
     
+     /** Initialize your data structure here. */
     public MyStack() {
-        primaryQueue = new LinkedList<>();
-        secondaryQueue = new LinkedList<>();
+        primaryQueue = new LinkedList<>(); // Primary queue for stack operations
+        secondaryQueue = new LinkedList<>(); // Secondary queue for temporary storage
     }
     
+      /** Push element x onto stack. */
     public void push(int x) {
-        primaryQueue.add(x);
+        primaryQueue.add(x); // Simply add the new element to the primary queue
     }
     
+     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
         while (primaryQueue.size() > 1) {
-            secondaryQueue.add(primaryQueue.poll());
+            secondaryQueue.add(primaryQueue.poll()); // Move elements from primary to       secondary queue except the last one
         }
-        int result = primaryQueue.poll();
-        swapQueues();
+        int result = primaryQueue.poll();  // The last element in the primary queue is the one to be removed
+        swapQueues(); // Swap the primary and secondary queues to maintain the order
         return result;
     }
     
+     /** Get the top element. */
     public int top() {
         while (primaryQueue.size() > 1) {
-            secondaryQueue.add(primaryQueue.poll());
+            secondaryQueue.add(primaryQueue.poll()); // Move elements from primary to secondary queue except the last one
         }
-        int result = primaryQueue.poll();
-        secondaryQueue.add(result);
-        swapQueues();
+        int result = primaryQueue.poll(); // The last element in the primary queue is the top element
+        secondaryQueue.add(result); // Add the top element back to the secondary queue
+        swapQueues(); // Swap the primary and secondary queues to maintain the order
         return result;
     }
     
+      /** Returns whether the stack is empty. */
     public boolean empty() {
-        return primaryQueue.isEmpty();
+        return primaryQueue.isEmpty(); // The stack is empty if the primary queue is empty
     }
     
+     /** Swap the two queues. */
     private void swapQueues() {
         Queue<Integer> temp = primaryQueue;
         primaryQueue = secondaryQueue;
