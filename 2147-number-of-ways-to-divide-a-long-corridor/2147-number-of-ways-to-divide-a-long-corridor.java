61a9094c3c0b0e22278e4a636d49d6d4e644a9c0
Gangavaram Thousif <91539305+Thousifg@users.noreply.github.com>
2023-11-28 23:38:04 +0530

    Time: 24 ms (84.21%), Space: 44.9 MB (65.79%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -1,34 +1,43 @@
 class Solution {
     public int numberOfWays(String corridor) {
-        int totalSeats = 0;
-        int totalWays = 1;
-        int n = corridor.length();
-        int mod = 1000000007;
+        // Step 1: Initialization
+        int totalSeats = 0;                 // Total number of seats in the corridor
+        int totalWays = 1;                  // Total number of ways to install dividers
+        int currentSeatCount = 0;           // Current count of seats between dividers
+        int currentPlantCount = 0;          // Current count of plants between dividers
+        boolean isFirstDividerInstalled = false;  // Flag to check if the first divider is installed
+        int mod = 1000000007;               // Constant for modulo operation
 
-        int currentSeatCount = 0;
-        int currentPlantCount = 0;
-        boolean isFirstDividerInstalled = false;
-
-        for (int i = 0; i < n; i++) {
+        // Step 2: Iterate through the Corridor
+        for (int i = 0; i < corridor.length(); i++) {
             char ch = corridor.charAt(i);
 
+            // If the current character is 'S', increment total seats and current seat count
             if (ch == 'S') {
                 totalSeats++;
                 currentSeatCount++;
-            } else if (currentSeatCount == 0) {
+            } 
+            // If the current character is 'P' and no seats are encountered yet, increment current plant count
+            else if (currentSeatCount == 0) {
                 currentPlantCount++;
             }
 
+            // Step 3: Install Dividers
             if (currentSeatCount == 2) {
+                // If the first divider is installed, update total ways based on current plant count
                 if (isFirstDividerInstalled) {
                     totalWays = (int) ((long) totalWays * (currentPlantCount + 1) % mod);
                 }
+                // Set the flag to indicate the first divider is installed
                 isFirstDividerInstalled = true;
+                // Reset current seat count and plant count for the next divider
                 currentSeatCount = 0;
                 currentPlantCount = 0;
             }
         }
 
+        // Step 4: Check for Valid Configuration
+        // If the total number of seats is even, return total ways modulo the constant
         return (totalSeats > 0 && totalSeats % 2 == 0) ? totalWays % mod : 0;
     }
 }
