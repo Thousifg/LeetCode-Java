93f149443ad889b90267c34a2f00dcef319c7d42
Gangavaram Thousif <91539305+Thousifg@users.noreply.github.com>
2023-12-02 23:57:13 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (36.07%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,14 +1,17 @@
 class Solution {
+    // Method to check if two arrays of strings represent equal words
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         // Concatenate strings in word1 and word2
         String left = concatenateStrings(word1);
         String right = concatenateStrings(word2);
 
-        // Use equals() method to compare strings
+        // Use equals() method to compare the concatenated strings
         return left.equals(right);
     }
 
+    // Helper method to concatenate strings in an array
     private String concatenateStrings(String[] words) {
+        // Use StringBuilder for efficient string concatenation
         StringBuilder result = new StringBuilder();
 
         // Concatenate strings in the array
@@ -16,6 +19,7 @@ class Solution {
             result.append(word);
         }
 
+        // Convert StringBuilder to String and return
         return result.toString();
     }
 }
