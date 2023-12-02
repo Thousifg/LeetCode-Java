011a307c2f22df904437f3fe60c31921842a510e
Gangavaram Thousif <91539305+Thousifg@users.noreply.github.com>
2023-12-02 23:54:48 +0530

    Time: 39 ms (27.53%), Space: 44.1 MB (69.38%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,19 +1,28 @@
 class Solution {
+    // Main method to calculate the total length of valid words
     public int countCharacters(String[] words, String chars) {
+        // Calculate character counts for the given characters
         Map<Character, Integer> charCounts = calculateCharacterCounts(chars);
 
+        // Initialize total length
         int totalLength = 0;
+
+        // Iterate through each word and check if it's valid
         for (String word : words) {
+            // Calculate character counts for the current word
             Map<Character, Integer> wordCounts = calculateCharacterCounts(word);
 
+            // Check if the word is valid and update the total length
             if (isWordValid(charCounts, wordCounts)) {
                 totalLength += word.length();
             }
         }
 
+        // Return the total length of valid words
         return totalLength;
     }
 
+    // Helper method to calculate character counts in a string
     private Map<Character, Integer> calculateCharacterCounts(String str) {
         Map<Character, Integer> counts = new HashMap<>();
         for (char c : str.toCharArray()) {
@@ -22,15 +31,17 @@ class Solution {
         return counts;
     }
 
+    // Helper method to check if a word is valid based on character counts
     private boolean isWordValid(Map<Character, Integer> charCounts, Map<Character, Integer> wordCounts) {
         for (Map.Entry<Character, Integer> entry : wordCounts.entrySet()) {
             char c = entry.getKey();
             int wordCount = entry.getValue();
 
+            // Check if the character count for the word is greater than or equal to the available count
             if (charCounts.getOrDefault(c, 0) < wordCount) {
-                return false;
+                return false; // Word is not valid
             }
         }
-        return true;
+        return true; // Word is valid
     }
 }
