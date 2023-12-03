c53313a2f2ac234e61aaf493f07ef00e04be5321
Gangavaram Thousif <91539305+Thousifg@users.noreply.github.com>
2023-12-03 23:59:31 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (34.74%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -1,17 +1,25 @@
 class Solution {
+    // Calculate the time needed to move from one point to another
     private int calculateTime(int[] from, int[] to) {
+        // Calculate the absolute difference in x and y coordinates
         int xDiff = Math.abs(from[0] - to[0]);
         int yDiff = Math.abs(from[1] - to[1]);
+
+        // The maximum of xDiff and yDiff represents the time to move in the greater direction
         return Math.max(xDiff, yDiff);
     }
 
+    // Calculate the minimum time to visit all points in the given array
     public int minTimeToVisitAllPoints(int[][] points) {
         int totalTime = 0;
 
+        // Iterate through the array starting from the second point
         for (int i = 1; i < points.length; i++) {
+            // Calculate the time to move from the previous point to the current point
             totalTime += calculateTime(points[i - 1], points[i]);
         }
 
+        // The accumulated totalTime represents the minimum time to visit all points
         return totalTime;
     }
 }
